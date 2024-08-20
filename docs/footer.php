1531447884bb2247ccd5c5671790dd435e14a689
reGv3 <162887558+reGv3@users.noreply.github.com>
2024-08-20 21:07:18 +0900

    Add files via upload

---
+++ docs/footer.php
@@ -0,0 +1,10 @@
+<!-- フッター -->
+
+	<div class="footer">
+		<div class="copyright">
+		</div>
+		<div class="footer_sitemap">
+			<a href="http://sc.local/新着情報/">ホーム</a> | <a href="#">リンク</a> | <a href="http://sc.local/contact/">問い合わせ</a>
+		</div>
+	</div>
+	<!-- /フッター -->
---
+++ docs/header.php
@@ -0,0 +1,22 @@
+<!-- ヘッダー -->
+
+	<div class="header">
+		<div class="header_primary">
+			<div id="logo">
+		
+			</div>
+			<div class="header_sub">
+				<div class="header_tel">スタジオクリエート</div>
+				<div class="header_submenu">
+				</div>
+				<h1 id="header_copy">テスト
+				</h1>
+			</div>
+		</div>
+		<ul class="nav">
+			<li><a href="http://sc.local/新着情報/">ホーム</a></li>
+			<li><a href="#">リンク</a></li>
+			<li><a href="http://sc.local/contact/">問い合わせ</a></li>
+		</ul>
+	</div>
+	<!-- /ヘッダー -->
